e75f399956f57d35a90993d297ee5368948220f3
watahiki606 <v10acdict@gmail.com>
2023-04-12 07:26:06 +0900

    init

---
+++ RailFinder/RailFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailFinderApp.swift
+//  RailFinder
+//
+//  Created by 綿引慎也 on 2023/04/12.
+//
+
+import SwiftUI
+
+@main
+struct RailFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
