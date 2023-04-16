97d8add2c6f60a08a3187479a90beb103a343c5e
watahiki606 <v10acdict@gmail.com>
2023-04-17 07:31:08 +0900

    change button size

--- RailFinder/NearestStationView.swift
+++ RailFinder/NearestStationView.swift
@@ -1,36 +1,45 @@
 import SwiftUI
 import CoreLocation
-
 struct NearestStationView: View {
     @StateObject private var locationManager = LocationManager.shared
     
     var body: some View {
         ZStack {
-            if (!locationManager.isLoading) {
-                VStack {
-                    Text(locationManager.nearestStation)
-                        .padding()
-                    
-                    HStack{
-                        Image(systemName: "train.side.front.car")
-                        Button("最寄り駅を検索")  {
-                            locationManager.startUpdatingLocation()
-                        }
-                    }
-                }
-                
-            } else {
+            if (locationManager.isLoading) {
                 ProgressView("searching...")
                     .progressViewStyle(CircularProgressViewStyle())
                 
+            } else {
+                VStack {
+                    Spacer()
+                    Text(locationManager.nearestStation)
+                        .padding()
+                    Button(action: {
+                        locationManager.startUpdatingLocation()
+                    }, label: {
+                        HStack {
+                            Image(systemName: "train.side.front.car")
+                            Text("検索")
+                        }
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .cornerRadius(10)
+                    })
+                    .padding()
+                    
+                    Spacer()
+                }
+                .onAppear {
+                    locationManager.requestPermission()
+                }
             }
-        }
-        
-        .onAppear {
-            locationManager.requestPermission()
         }
     }
 }
+
+
 
 
 struct FullScreenButtonView: View {
