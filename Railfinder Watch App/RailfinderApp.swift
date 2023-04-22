4d5f69c1f20d0d893130766bfe6caa1d0dfbddb6
watahiki606 <v10acdict@gmail.com>
2023-04-22 15:33:01 +0900

    add watch target

---
+++ Railfinder Watch App/RailfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailfinderApp.swift
+//  Railfinder Watch App
+//
+//  Created by 綿引慎也 on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Railfinder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
